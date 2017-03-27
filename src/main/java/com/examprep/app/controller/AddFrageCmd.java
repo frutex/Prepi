79326eba3483c6602bfbefbe997e8d703b165c45
Daniel Koehler <daniel.koehler@de.ibm.com>
2017-03-27 20:48:46 +0200

    Further Impl works
    Works on Neue Klausurfrage

--- src/main/java/com/examprep/app/controller/AddFrageCmd.java
+++ src/main/java/com/examprep/app/controller/AddFrageCmd.java
@@ -1,8 +1,15 @@
 package com.examprep.app.controller;
 
+import java.util.Date;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.examprep.app.bean.Hochschule;
+import com.examprep.app.bean.Modul;
+import com.examprep.app.bean.Nutzer;
+import com.examprep.app.persistencelayer.PersistenceQuery;
 
 public class AddFrageCmd extends AbstractCmdServlet{
 
@@ -14,7 +21,24 @@ public class AddFrageCmd extends AbstractCmdServlet{
 	@Override
 	public void execute() throws Exception {
 		
+		String nutzer = request.getParameter("token").split("\\|")[0];
+		String titel = request.getParameter("titel");
+		String hochschule = request.getParameter("hochschule");
+		String dozent = request.getParameter("dozent");
+		String modul = request.getParameter("modul");
+		String beschreibung = request.getParameter("beschreibung");
+		String datum = request.getParameter("datum");
 		
+		//Date da = new Date(Integer.parseInt(datum.split(".")[2]), datum.split(".")[1], datum.split(".")[0])
+		@SuppressWarnings("deprecation")
+		Date dat = new Date(datum);
+	
+		Nutzer n = PersistenceQuery.getNutzerByName(nutzer).get(0);
+	//	Dozent d = PersistenceQuery.get
+		Hochschule h = PersistenceQuery.searchForHochschulenByName(hochschule).get(0);
+		Modul m = PersistenceQuery.searchModulByName(modul).get(0);
+		
+		//PersistenceQuery.createKlausurfrage( beschreibung, dat.getYear(), hochschule, dozent, modul, nutzer)
 		
 		
 	}
--- src/main/webapp/js/requestFactory.js
+++ src/main/webapp/js/requestFactory.js
@@ -6,7 +6,9 @@ angular.module('ExPrep').factory(
 				'AuthService',
 				function($http, $filter, AuthService) {
 					function send(url) {
-						return $http.post("/prepa/cmd?" + url + '&token=' + AuthService.getUserToken()).success(
+						return $http.post(
+								"/prepa/cmd?" + url + '&token='
+										+ AuthService.getUserToken()).success(
 								function(data) {
 									return data;
 								})
@@ -17,38 +19,39 @@ angular.module('ExPrep').factory(
 								+ "&password=" + password);
 
 					}
-					
+
 					function getHochschulen() {
-						return send ("c=getHochschulen");
+						return send("c=getHochschulen");
 
 					}
-					
+
 					function checkAuthToken() {
 						return send("c=checkAuthToken");
 
 					}
-					
+
 					function loadUserData() {
 						return send("c=loadUserData");
 
 					}
-					
+
 					function getModule() {
 						return send("c=getModule");
 
 					}
-					
+
 					function getDozenten() {
 						return send("c=getDozenten");
 
 					}
-					
-					function addFrage(data) {
-						return send("c=addFrage" + "&titel=" + data.Titel + "&hochschule=" + data.Hochschule + "&modul=" + data.Modul + "&dozent=" + data.Dozent + "&beschreibung=" + data.Beschreibung + "&datum=" + data.Datum);
 
-								
-										
-										
+					function addFrage(data) {
+						return send("c=addFrage" + "&titel=" + data.Titel
+								+ "&hochschule=" + data.Hochschule + "&modul="
+								+ data.Modul + "&dozent=" + data.Dozent
+								+ "&beschreibung=" + data.Beschreibung
+								+ "&datum=" + data.Datum);
+
 					}
 
 					return {
@@ -60,6 +63,6 @@ angular.module('ExPrep').factory(
 						getModule : getModule,
 						getDozenten : getDozenten,
 						addFrage : addFrage,
-						
+
 					};
 				} ]);
