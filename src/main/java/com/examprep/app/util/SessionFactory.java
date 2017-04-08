a06d605ee5a3a34bbd24ba9edf9baf8f622b0292
Unknown <bjarne.obst@gmail.com>
2017-04-08 21:58:30 +0200

    test

--- src/main/java/com/examprep/app/util/SessionFactory.java
+++ src/main/java/com/examprep/app/util/SessionFactory.java
@@ -3,6 +3,7 @@ package com.examprep.app.util;
 import java.sql.SQLException;
 
 import com.j256.ormlite.jdbc.JdbcConnectionSource;
+import com.j256.ormlite.jdbc.JdbcPooledConnectionSource;
 import com.j256.ormlite.support.ConnectionSource;
 
 public class SessionFactory {
@@ -11,7 +12,7 @@ public class SessionFactory {
 	private static String connURL = "jdbc:mysql://rds-mysql-10-min-tutorial.cn3d9reirkc0.eu-central-1.rds.amazonaws.com:3306/MyTestDB";
 	private static String connUsername = "masterUsername";
 	private static String connPassword = "masterUsername";
-	ConnectionSource connSource = null;
+	JdbcPooledConnectionSource connSource = null;
 	
 	public SessionFactory(){
 		
@@ -19,7 +20,14 @@ public class SessionFactory {
 	
 	public ConnectionSource createConnection(){
 		try {
-			connSource = new JdbcConnectionSource(connURL, connUsername, connPassword);
+
+			connSource = new JdbcPooledConnectionSource(connURL, connUsername, connPassword);
+			connSource.setMaxConnectionAgeMillis(60*1000);
+			connSource.setCheckConnectionsEveryMillis(10*1000);
+			// for extra protection, enable the testing of connections
+			// right before they are handed to the user
+			connSource.setTestBeforeGet(true);
+			
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
